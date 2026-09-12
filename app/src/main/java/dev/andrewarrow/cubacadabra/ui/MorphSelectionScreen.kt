7e9bcb3afa52fbc9972afe0ec6f8de05dbc24fd3
aa <oneone@gmail.com>
2026-09-12 06:19:22 -0700

    fix compile

--- app/src/main/java/dev/andrewarrow/cubacadabra/ui/MorphSelectionScreen.kt
+++ app/src/main/java/dev/andrewarrow/cubacadabra/ui/MorphSelectionScreen.kt
@@ -13,7 +13,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Scaffold
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
@@ -35,6 +34,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
